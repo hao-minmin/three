3dba42c37d2b4753a5b4e40372d6b3900b987eed
haoxiaomin <2645627070@qq.com>
2020-04-04 23:00:31 +0800

    单元测试

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -3,10 +3,15 @@ import sys
 import click
 
 from flask import Flask,url_for,render_template,request,url_for,redirect,flash
+# 数据库
 from flask_sqlalchemy import SQLAlchemy 
+# 生成密码 验证密码
+from werkzeug.security import generate_password_hash,check_password_hash
+# 登录
+from flask_login import LoginManager,UserMixin,login_user,logout_user,login_required,current_user
+
 
 # 得到当前平台
-
 WIN = sys.platform.startswith('win')
 if WIN:
     # 请求头
@@ -25,18 +30,43 @@ app.config['SECRET_KEY']='dev'
 # 传入app实例 在配置之后
 db=SQLAlchemy(app)
 
-class User(db.Model):
+# 实例化登录扩展类  
+login_manager = LoginManager(app)
+# 用户加载的函数
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
+login_manager.login_view='login'
+login_manager.login_manage="您未登录"
+
+
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+    # 生成密码
+    def set_password(self,password):
+        self.password_hash=generate_password_hash(password)
+    
+    # 验证密码
+    def validate_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+
 class Movie(db.Model):
     id= db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
 
 
+# 表单添加
 @app.route("/",methods=['GET','POST'])
 def index():
     if request.method =="POST":
+        if not current_user.is_authenticated:
+            return redirect(url_for("index"))
         name = request.form.get("movie_name")
         year = request.form.get("movie_year")
         # 验证数据不为空  year长度不能超过4 name 不能超过60
@@ -48,31 +78,25 @@ def index():
         db.session.commit()
         flash("数据插入成功")
         return redirect(url_for("index"))
-
-
-
     movie = Movie.query.all()  #读取所有的电影记录 
-
     return render_template("index.html",movies=movie)
 
 
-# 编辑视图函数 
+# 表单编辑 
 @app.route('/movie/edit/<int:movies_id>',methods=['GET','POST'])
+@login_required
 def edit(movies_id):
     mm = Movie.query.get_or_404(movies_id)
     print(mm)
     if request.method=="POST":
         title = request.form['movie_name']
         year = request.form['movie_year']
-
         # 验证数据不为空  year长度不能超过4 name 不能超过60
         if not title or not year or len(year)>4 or len(title)>60:
             flash("输入有误")
             return redirect(url_for("edit"),movie_id=movies_id)
-
         mm.title =title
         mm.year = year
-
         # db.session.add(movie)
         db.session.commit()
         flash("数据编辑成功")
@@ -80,16 +104,65 @@ def edit(movies_id):
     return render_template("edit.html",movie=mm)
 
 
-# 删除
+# 表单删除
 @app.route('/movie/delete/<int:movies_id>',methods=['POST'])
-
+@login_required
 def delete(movies_id):
     movie = Movie.query.get_or_404(movies_id)
-
     db.session.delete(movie)
     db.session.commit()
     flash("数据删除成功")
     return redirect(url_for('index'))
+
+
+
+# 登录
+@app.route('/login',methods=["GET","POST"])
+def login():
+    if request.method =="POST":
+        username = request.form['username']
+        password = request.form['password']
+        if not username or not password:
+            flash("输入错误")
+            return redirect(url_for('login'))
+        user = User.query.first()
+        # 验证用户名和密码是否一致
+        if username==user.username and user.validate_password(password):
+            login_user(user)
+            flash("登录成功")
+            return redirect(url_for('index'))
+        flash('用户名或密码错误')
+        return redirect(url_for('login'))
+    return render_template('login.html')
+
+
+
+# 登出
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash("退出成功")
+    return redirect(url_for('index'))
+
+
+
+# 设置
+@app.route('/settings',methods=["GET","POST"])
+@login_required
+def settings():
+    if request.method=='POST':
+        name = request.form['name']
+        if not name or len(name)>20:
+            flash("输入有误")
+            return redirect("settings")
+        current_user.name=name
+        # 等于下面两句
+        # user = User.query.first()
+        # user.name = name
+        db.session.commit()
+        flash("设置name成功")
+        return redirect(url_for('index'))
+    return render_template('settings.html')
 
 
 #     #动态url 
@@ -97,7 +170,35 @@ def delete(movies_id):
 # def home(name):
 #     print(url_for("home",name="haominmin"))
 #     return "<h1>hello,%s</h1>"%name
-# 自定义命令 
+
+
+
+# 自定义命令 创建用户的
+@app.cli.command()
+# 接收用户名   prompt=True提示  
+@click.option('--username',prompt=True,help='管理员账号') 
+# 接收密码  再次确认密码 需要输入两次 confirmation_prompt=True  hide_input=True秘文
+@click.option('--password',prompt=True,help='密码',confirmation_prompt=True,hide_input=True)
+def admin(username,password):
+    db.create_all()
+    user = User.query.first()
+    # 如果存在
+    if user is not None:
+        click.echo('更新用户')
+        user.username=username
+        user.set_password(password)
+        # user.password_hash=password
+    else:
+        click.echo('创建用户')
+        user = User(username=username,name='minmin')
+        user.set_password(password)
+        db.session.add(user)
+    db.session.commit()
+    click.echo("完成")
+
+
+
+# 初始化数据库
 @app.cli.command() #装饰器注册命令
 @click.option('--drop',is_flag=True,help='删除之后再创建')
 def initdb(drop):
@@ -109,7 +210,9 @@ def initdb(drop):
 
     click.echo("初始化数据库完成")
 
-# 自定义命令 装饰器
+
+
+# 自定义命令导入数据
 @app.cli.command()
 def forge():
     # 初始化数据库
@@ -133,13 +236,14 @@ def forge():
     click.echo("导入数据完成")
 
 
-# 错误处理 
+# 错误处理 404
 @app.errorhandler(404)
 def page_not_found(e):
     # user = User.query.first()
     # return render_template("404.html",user=user),404
     # 返回模板和状态码
     return render_template("404.html"),404
+
 
 # 模板上下文处理函数
 @app.context_processor
---
+++ day1/watchlist/test_project.py
@@ -0,0 +1,97 @@
+import unittest   
+
+from app import app,db,Movie,User
+
+class ProjectTestCase(unittest.TestCase):
+    #测试固件 两个不同的函数
+
+    def setUp(self):
+        #调用app
+
+        app.config.update(
+            #处于测试环境
+            TESTING=True,
+            #用缓存数据库  memory 内存数据库
+            SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+
+        )
+
+        # 往库中
+
+        db.create_all()
+        user = User(name="Test",username="test")
+        user.set_password("123456")
+
+        movie = Movie(title="Test Movie Title",year="2020")
+        #add一条上传  add_all 多条列表上传
+        db.session.add_all([user,movie])
+
+        db.session.commit()
+        
+        # 创建一个测试用的客户端模拟 浏览器  
+        self.client = app.test_client()
+
+        #创建测试命令运行器
+        self.runner = app.test_cli_runner()
+
+    def testDown(self):
+        #数据库清除
+
+        db.session.remove()
+        db.drop_all()
+
+    def test_app_exist(self):
+        #测试app是不是空的
+
+        self.assertIsNotNone(app)
+        
+    #测试是否处于测试模式
+    def test_app_is_testing(self):
+        self.assertTrue(app.config["TESTING"])
+
+    #测试404页面是否存在
+    def test_404_page(self):
+        #通过找页面特征来判断
+        response = self.client.get('/hhh')  #传入一个不存在的url 
+
+        data= response.get_data(as_text=True)
+
+        self.assertIn("页面跑丢了 ~~ 404",data)
+
+        self.assertIn("返回首页",data)
+
+        #状态码 
+        self.assertEqual(response.status_code,404)
+#测试主页
+    def test_index_page(self):
+        response = self.client.get('/')
+        data=  response.get_data(as_text=True)
+
+        self.assertIn("Test's watchlist",data)
+        self.assertIn('Test Movie Title',data)
+
+        self.assertEqual(response.status_code,200)
+
+#测试登录登出  增删改查 测试命令运行器
+
+if __name__=="__main__":
+    unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
